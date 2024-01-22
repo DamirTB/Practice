c2c09ec6746582f43ced037fd949abd95272a98d
DamirTB <talipdamir@gmail.com>
2024-01-23 00:39:00 +0600

    Added unit tests

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"html/template"
-	_ "io/fs" // New import
+	"io/fs" // New import
 	"path/filepath"
 	"time"
 	"snippetbox.alexedwards.net/internal/models"
-	_ "snippetbox.alexedwards.net/ui" // New import
+	"snippetbox.alexedwards.net/ui" // New import
 )
 
 type templateData struct {
@@ -20,7 +20,12 @@ type templateData struct {
 }
 
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	// Return the empty string if time has the zero value.
+	if t.IsZero() {
+	return ""
+	}
+	// Convert the time to UTC before formatting it.
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
@@ -29,25 +34,26 @@ var functions = template.FuncMap{
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	// Use fs.Glob() to get a slice of all filepaths in the ui.Files embedded
+	// filesystem which match the pattern 'html/pages/*.tmpl'. This essentially
+	// gives us a slice of all the 'page' templates for the application, just
+	// like before.
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 	if err != nil {
 	return nil, err
 	}
 	for _, page := range pages {
 	name := filepath.Base(page)
-	// The template.FuncMap must be registered with the template set before you
-	// call the ParseFiles() method. This means we have to use template.New() to
-	// create an empty template set, use the Funcs() method to register the
-	// template.FuncMap, and then parse the file as normal.
-	ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
-	if err != nil {
-	return nil, err
+	// Create a slice containing the filepath patterns for the templates we
+	// want to parse.
+	patterns := []string{
+		"html/base.html",
+		"html/partials/*.html",
+	page,
 	}
-	ts, err = ts.ParseGlob("./ui/html/partials/*.html")
-	if err != nil {
-	return nil, err
-	}
-	ts, err = ts.ParseFiles(page)
+	// Use ParseFS() instead of ParseFiles() to parse the template files
+	// from the ui.Files embedded filesystem.
+	ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 	if err != nil {
 	return nil, err
 	}
